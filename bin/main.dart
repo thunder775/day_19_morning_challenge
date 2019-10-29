291418af2b042bf0c940eb19c03e1958a784e9b4
McLarenCollege <hello@mclarencollege.in>
2019-10-29 08:23:24 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,25 @@
+// Challenge 1
+// Write failing test cases for given 3 challenges
+
+// Challenge 2
+// Sort by Digit Length, then By Number Itself
+// Write a function that sorts an array of integers by their digit length in
+// descending order, then settles ties by sorting numbers with the same digit length in ascending order.
+
+// Challenge 3
+// IPv4 Validation
+// Create a function that takes a string (IPv4 address in standard dot-decimal
+// format) and returns true if the IP is valid or false if it's not.
+// isValidIP("1.2.3.4") ➞ true
+// isValidIP("1.2.3") ➞ false
+// isValidIP("123.045.067.089") ➞ false
+
+// Challenge 4
+// Double Character Swap
+// Write a function to replace all instances of character c1 with character c2 and vice versa.
+// Examples
+// doubleSwap( "aabbccc", "a", "b") ➞ "bbaaccc"
+
+main() {
+
+}
