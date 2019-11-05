ee2ae909c88fb154ed77992fe69d23ef4d5831ab
thunder775 <rahulmasih61@gmail.com>
2019-11-05 16:26:13 +0530

    done

--- bin/test.dart
+++ bin/test.dart
@@ -1,7 +1,14 @@
 import 'package:test/test.dart';
 import 'main.dart';
  main(){
+   test('digit length', (){
+    expect( digitLength([9,33, 44, 1, 22]),
+        [22, 33, 44, 1, 9]) ;
+   });
    test('replacing characters', (){
-    expect(characterSwap('aabbcc','a','c'),'ccbbaa' ) ;
+     expect(characterSwap('aabbcc','a','c'),'ccbbaa' ) ;
+   });
+   test('valid IP', (){
+     expect(isValidIP("123.045.06.70"),true ) ;
    });
  }
